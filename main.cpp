e767fa1d250a21ab8fafe29ba0cca6a6ef657d91
Vlad <Vlad@DESKTOP-B9NF7M3>
2024-10-29 17:52:29 +0200

    Rezolvare

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,25 +9,44 @@ int main()
 {
     ifstream inFile("input.txt");
 
+    struct q
+    {
+        string name;
+        string specialty;
+    };
+
     int no_problems, no_doctors;
-    string name, speciality;
-    
+
     inFile >> no_problems;
+
+    vector<q> problems(no_problems);
 
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> problems[i].name;
+        inFile >> problems[i].specialty;
     }
 
     inFile >> no_doctors;
 
+    vector<q> doctors(no_doctors);
+
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> doctors[i].name;
+        inFile >> doctors[i].specialty;
+    }
+
+    for (int i = 0; i < no_problems; i++)
+    {
+        int verif = 1;
+        for (int j = 0; j < no_doctors && verif; j++)
+        {
+            if (doctors[j].specialty == problems[i].specialty) verif = 0;
+        }
+
+        if (verif) cout << problems[i].name << " " << "Respins" << endl;
+        else cout << problems[i].name << " " << "Acceptat" << endl;
     }
 
     return 0;
